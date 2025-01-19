ca2b689b03f239a4128451222759c16caebf6636
nireo <eemil.lehtikangas1@gmail.com>
2025-01-19 20:00:01 +0200

    add skiplist tests and cleanup code

--- tests/skiplist_test.c
+++ tests/skiplist_test.c
@@ -4,14 +4,17 @@
 #if defined(__MACH__) && !defined(CLOCK_REALTIME)
 #include <sys/time.h>
 
-#define CLOCK_REALTIME 0
+#define CLOCK_REALTIME  0
 #define CLOCK_MONOTONIC 1
 
-int clock_gettime(int clk_id, struct timespec* t) {
+int
+clock_gettime(int clk_id, struct timespec* t)
+{
     struct timeval now;
     int rv = gettimeofday(&now, NULL);
-    if (rv) return rv;
-    t->tv_sec  = now.tv_sec;
+    if (rv)
+        return rv;
+    t->tv_sec = now.tv_sec;
     t->tv_nsec = now.tv_usec * 1000;
     return 0;
 }
@@ -21,41 +24,44 @@ int clock_gettime(int clk_id, struct timespec* t) {
 
 #include "../skiplist.h"
 
-#define N 1024 * 1024  // Reduced size for string test
+#define N                 1024 * 1024 // Reduced size for string test
 #define MAX_STRING_LENGTH 16
 
-static void generate_random_string(char *str, size_t length) {
+static void
+generate_random_string(char* str, size_t length)
+{
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     if (length) {
-        for (size_t i = 0; i < length-1; i++) {
+        for (size_t i = 0; i < length - 1; i++) {
             int key = random() % (sizeof(charset) - 1);
             str[i] = charset[key];
         }
-        str[length-1] = '\0';
+        str[length - 1] = '\0';
     }
 }
 
-int main(void) {
+int
+main(void)
+{
     struct timespec start, end;
-    char **keys = (char **)malloc(N * sizeof(char *));
-    char **values = (char **)malloc(N * sizeof(char *));
-    
+    char** keys = (char**)malloc(N * sizeof(char*));
+    char** values = (char**)malloc(N * sizeof(char*));
+
     if (keys == NULL || values == NULL) {
         fprintf(stderr, "Memory allocation failed\n");
         exit(-1);
     }
 
-    // Allocate memory for each string
     for (int i = 0; i < N; i++) {
-        keys[i] = (char *)malloc(MAX_STRING_LENGTH);
-        values[i] = (char *)malloc(MAX_STRING_LENGTH);
+        keys[i] = (char*)malloc(MAX_STRING_LENGTH);
+        values[i] = (char*)malloc(MAX_STRING_LENGTH);
         if (keys[i] == NULL || values[i] == NULL) {
             fprintf(stderr, "Memory allocation failed\n");
             exit(-1);
         }
     }
 
-    struct skiplist *list = skiplist_new();
+    skiplist* list = skiplist_new();
     if (list == NULL) {
         fprintf(stderr, "Skiplist creation failed\n");
         exit(-1);
@@ -64,7 +70,6 @@ int main(void) {
     printf("Test start!\n");
     printf("Adding %d nodes with string keys and values...\n", N);
 
-    /* Insert test */
     srandom(time(NULL));
     clock_gettime(CLOCK_MONOTONIC, &start);
     for (int i = 0; i < N; i++) {
@@ -73,22 +78,14 @@ int main(void) {
         skiplist_insert(list, keys[i], values[i]);
     }
     clock_gettime(CLOCK_MONOTONIC, &end);
-    printf("Insertion time: %ldms\n", 
-           (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
+    printf("Insertion time: %ldms\n",
+        (end.tv_sec - start.tv_sec) * 1000 + (end.tv_nsec - start.tv_nsec) / 1000000);
 
-    #ifdef SKIPLIST_DEBUG
-    skiplist_dump(list);
-    #endif
-
-    /* Search test by key */
     printf("Searching each node by key...\n");
     clock_gettime(CLOCK_MONOTONIC, &start);
     for (int i = 0; i < N; i++) {
-        struct skipnode *node = skiplist_search_by_key(list, keys[i]);
+        skipnode* node = skiplist_search_by_key(list, keys[i]);
         if (node != NULL) {
-            #ifdef SKIPLIST_DEBUG
-            printf("Found - Key: %s, Value: %s\n", node->key, node->value);
-            #endif
             if (strcmp(node->value, values[i]) != 0) {
                 printf("Error: Value mismatch for key %s\n", keys[i]);
             }
@@ -98,27 +95,25 @@ int main(void) {
     }
     clock_gettime(CLOCK_MONOTONIC, &end);
     printf("Search by key time: %ldms\n",
-           (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
+        (end.tv_sec - start.tv_sec) * 1000 + (end.tv_nsec - start.tv_nsec) / 1000000);
 
-    /* Range search test */
     if (N > 1) {
         printf("Testing range search...\n");
-        struct range_spec range;
+        range_spec range;
         range.min = keys[0];
         range.max = keys[1];
-        range.minex = 0;
-        range.maxex = 0;
-        
+        range.min_exclusive = 0;
+        range.max_exclusive = 0;
+
         clock_gettime(CLOCK_MONOTONIC, &start);
         if (key_in_range(list, &range)) {
             printf("Found keys in range [%s, %s]\n", range.min, range.max);
         }
         clock_gettime(CLOCK_MONOTONIC, &end);
         printf("Range search time: %ldms\n",
-               (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
+            (end.tv_sec - start.tv_sec) * 1000 + (end.tv_nsec - start.tv_nsec) / 1000000);
     }
 
-    /* Delete test */
     printf("Removing all nodes...\n");
     clock_gettime(CLOCK_MONOTONIC, &start);
     for (int i = 0; i < N; i++) {
@@ -126,15 +121,10 @@ int main(void) {
     }
     clock_gettime(CLOCK_MONOTONIC, &end);
     printf("Deletion time: %ldms\n",
-           (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
-
-    #ifdef SKIPLIST_DEBUG
-    skiplist_dump(list);
-    #endif
+        (end.tv_sec - start.tv_sec) * 1000 + (end.tv_nsec - start.tv_nsec) / 1000000);
 
     printf("End of Test.\n");
-    
-    /* Cleanup */
+
     skiplist_delete(list);
     for (int i = 0; i < N; i++) {
         free(keys[i]);
