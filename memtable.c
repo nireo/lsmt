62d894cc5b1c9c046dcfcbc18fbbdd249ce053ec
nireo <eemil.lehtikangas1@gmail.com>
2025-01-19 20:13:49 +0200

    added basic memtable implementation

---
+++ memtable.c
@@ -0,0 +1,42 @@
+#include "memtable.h"
+#include "bloom.h"
+#include "skiplist.h"
+#include <string.h>
+
+memtable*
+memtable_new(size_t size)
+{
+  memtable* mt = malloc(sizeof(memtable));
+  if (mt == NULL) {
+    return NULL;
+  }
+
+  mt->bloom_filter = bloom_filter_new_default(size); 
+  mt->skiplist = skiplist_new();
+
+  return mt;
+}
+
+memtable_res memtable_insert(memtable *mt, const char *key, const char *value) {
+  bloom_filter_put_str(mt->bloom_filter, key);
+  skiplist_insert(mt->skiplist, key, value);
+
+  return MEMTABLE_OK;
+}
+
+memtable_res memtable_get(memtable *mt, const char *key, char **value) {
+  // not in bloom filter we can ignore this
+  if (!bloom_filter_test_str(mt->bloom_filter, key)) {
+    return MEMTABLE_FAILED;
+  }
+
+  skipnode *n = skiplist_search_by_key(mt->skiplist, key);
+  *value = strdup(n->value);
+  return MEMTABLE_OK;
+}
+
+void memtable_free(memtable *mt) {
+  bloom_filter_free(mt->bloom_filter);
+  skiplist_delete(mt->skiplist);
+  free(mt);
+} 
--- memtable.h
+++ memtable.h
@@ -2,10 +2,22 @@
 #define __MEMTABLE_H__
 
 #include "bloom.h"
+#include "skiplist.h"
 #include "utils.h"
 
+// TODO: make this better 
+typedef enum {
+  MEMTABLE_OK,
+  MEMTABLE_FAILED,
+} memtable_res;
+
 typedef struct {
-    bloom_filter *bloom_filter; // we can have this to speed up look ups.
+  bloom_filter* bloom_filter; // we can have this to speed up look ups.
+  skiplist* skiplist;
 } memtable;
+
+memtable* memtable_new(size_t size);
+memtable_res memtable_insert(memtable *mt, const char *key, const char *value);
+memtable_res memtable_get(memtable *mt, const char *key, char **value);
 
 #endif
